906195894e7958e09f4402467bfa51eafb2ff030
Leonid Lobanov <leonid.lobanov@depowise.com>
2026-02-08 19:06:50 +0200

    Just trying thigs

--- src/BeautifyBaltics.Core.API/Program.cs
+++ src/BeautifyBaltics.Core.API/Program.cs
@@ -70,13 +70,14 @@ internal class Program
         // Register exceptions handling
         builder.Services.AddDefaultExceptionHandler();
 
-        builder.Services.AddMartenDefaults(builder.Configuration, builder.Environment, null)
-            .ProcessEventsWithWolverineHandlersInStrictOrder("bookings", o =>
-            {
-                o.IncludeType<BookingCreated>();
-                o.IncludeType<BookingConfirmed>();
-                o.IncludeType<BookingCancelled>();
-            });
+        builder.Services.AddMartenDefaults(builder.Configuration, builder.Environment, null);
+            // TODO: Re-enable after deployment is stable
+            // .ProcessEventsWithWolverineHandlersInStrictOrder("bookings", o =>
+            // {
+            //     o.IncludeType<BookingCreated>();
+            //     o.IncludeType<BookingConfirmed>();
+            //     o.IncludeType<BookingCancelled>();
+            // });
 
         builder.Services.ConfigurePersistence();
 
